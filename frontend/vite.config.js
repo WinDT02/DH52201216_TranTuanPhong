14f3b8bdd5c44692f07c7c6714648752e70fea62
WinDT02 <153409721+WinDT02@users.noreply.github.com>
2025-12-15 13:22:34 +0700

    Setup deploy GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // THÊM DÒNG DƯỚI NÀY (Thay đúng tên Repo của bạn vào)
+  base: '/DH52201216_TranTuanPhong/', 
   server: {
-    host: true,  // QUAN TRỌNG
+    host: true,
     port: 5173
   }
 })
